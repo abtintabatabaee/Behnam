191d338fb4de3b6d4fac860ce964d988811050a4
abtintabatabaee <abtintabatabaeiabtin@gmail.com>
2025-11-26 21:02:20 +0330

    Updates

--- src/services/instance.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: 'https://mghjjzmyuuvrnydiaqcb.supabase.co/rest/v1',
-  timeout: 1000,
-  headers: {'X-Custom-Header': 'foobar',
-  }
-});
-export default instance;
